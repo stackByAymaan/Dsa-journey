c0d644f4c4cba29aa84429e96a04572a28c70600
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-08-29 20:36:49 +0530

    Updated java

--- 01_Java Basic/JavaBasics.java
+++ 01_Java Basic/JavaBasics.java
@@ -1,18 +1,19 @@
+import java.util.*;
+
 public class JavaBasics {
     public static void main(String[] args) {
-        byte myByte = 100;
-        short myShort = 10000;
-        int myInt = 100000;
-        long myLong = 100000L;
-        float myFloat = 10.5f;
-        double myDouble = 20.99;
-        char myChar = 'A';
-        System.out.println("Byte value: " + myByte);
-        System.out.println("Short value: " + myShort);
-        System.out.println("Integer value: " + myInt);
-        System.out.println("Long value: " + myLong);
-        System.out.println("Float value: " + myFloat);
-        System.out.println("Double value: " + myDouble);
-        System.out.println("Character value: " + myChar);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius of circle: ");
+        int rad = sc.nextInt();
+
+        Double pi = 3.14;
+
+        Double area = pi * rad * rad;
+
+        System.out.println("Area of circle : " + area);
+
+        sc.close();
     }
 }
