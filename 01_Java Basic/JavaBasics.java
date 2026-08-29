62388382b6ea7a9ad05148f76732fe14f4767c98
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-08-29 14:29:14 +0530

    Java Basics

---
+++ 01_Java Basic/JavaBasics.java
@@ -0,0 +1,18 @@
+public class JavaBasics {
+    public static void main(String[] args) {
+        byte myByte = 100;
+        short myShort = 10000;
+        int myInt = 100000;
+        long myLong = 100000L;
+        float myFloat = 10.5f;
+        double myDouble = 20.99;
+        char myChar = 'A';
+        System.out.println("Byte value: " + myByte);
+        System.out.println("Short value: " + myShort);
+        System.out.println("Integer value: " + myInt);
+        System.out.println("Long value: " + myLong);
+        System.out.println("Float value: " + myFloat);
+        System.out.println("Double value: " + myDouble);
+        System.out.println("Character value: " + myChar);
+    }
+}
