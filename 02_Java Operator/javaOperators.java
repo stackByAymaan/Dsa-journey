edf2751f606116a8033d28767289f9a928aeed06
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-01 00:30:52 +0530

    practice artihmetic operator

---
+++ 02_Java Operator/javaOperators.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class javaOperators {
+    public static void main(String[] args) {
+       int a = 10;
+       int b = 30;
+       System.out.println("add = " + (a+b));
+       System.out.println("sub = " + (a-b));
+       System.out.println("mul = " + (a*b));
+       System.out.println("div = " + (a/b));
+    }
+}
