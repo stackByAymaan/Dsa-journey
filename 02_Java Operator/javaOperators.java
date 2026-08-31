2a141b656a3e2fcf62949c22c6e504833cef1f55
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-01 00:45:11 +0530

    Practicing Assingment Opeartors

--- 02_Java Operator/javaOperators.java
+++ 02_Java Operator/javaOperators.java
@@ -4,7 +4,8 @@ public class javaOperators {
     public static void main(String[] args) {
        int a = 10;
        int b = 20;
-       System.out.println( a <= b || a == b);
+       System.out.println(a += 10);
+       System.out.println(a -= 10);
 
     }
 }
