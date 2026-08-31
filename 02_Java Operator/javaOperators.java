f2b2e7964fcb157895126a33ab140071dcf658b4
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-01 00:42:59 +0530

    practicing Logical Operators

--- 02_Java Operator/javaOperators.java
+++ 02_Java Operator/javaOperators.java
@@ -4,7 +4,7 @@ public class javaOperators {
     public static void main(String[] args) {
        int a = 10;
        int b = 20;
-       System.out.println( a <= b);
+       System.out.println( a <= b || a == b);
 
     }
 }
