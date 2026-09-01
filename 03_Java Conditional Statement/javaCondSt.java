1ba9f62507efed92dd26219da1a5470c5e59f9fc
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-01 23:21:18 +0530

    Largest among All

--- 03_Java Conditional Statement/javaCondSt.java
+++ 03_Java Conditional Statement/javaCondSt.java
@@ -3,15 +3,22 @@ import java.util.*;
 public class javaCondSt {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
 
-        if (num % 2 ==0) {
-            System.out.println(num + " is Even.");
-        } else if (num % 2 != 0) {
-            System.out.println(num + " is Odd.");
+        System.out.print("Enter A: ");
+        int A = sc.nextInt();
+
+        System.out.print("Enter B: ");
+        int B = sc.nextInt();
+
+        System.out.print("Enter C: ");
+        int C = sc.nextInt();
+
+        if (A > B && A > C) {
+            System.out.println(A + " is the largest number.");
+        } else if (B > A && B > C) {
+            System.out.println(B + " is the largest number.");
         } else {
-            System.out.println("The number is either zero or negative.");
+            System.out.println(C + " is the largest number.");
         }
 
         sc.close();
