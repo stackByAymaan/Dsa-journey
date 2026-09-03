0a77f169014d71299df4c9eaf3a6bced8ca326ed
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-03 23:13:23 +0530

    Update java

--- 04_Java Loops/Loop.java
+++ 04_Java Loops/Loop.java
@@ -4,20 +4,11 @@ public class Loop {
 
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
-        String even = "";
-        String odd = "";
-        for( int i = 1; i <= num; i++) {
-            if(i % 2 ==0) {
-                even = even + i + " ";
-            } else if(i % 2 != 0) {
-                odd = odd + i + " ";
-            }
-        }
-        System.out.println("Even = " + even );
-        System.out.println("Odd = " + odd);
-        sc.close();
+        
+    for (int i = 0; i < 4; i++) {
+        System.out.println("****");
+    }
+        
     }
 
 }
