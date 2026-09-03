00a9fd263215213601d0122e531b609377c3c8bf
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-03 23:16:18 +0530

    carazy p[attern

--- 04_Java Loops/Loop.java
+++ 04_Java Loops/Loop.java
@@ -2,13 +2,53 @@ import java.util.*;
 
 public class Loop {
 
-    public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-        
-    for (int i = 0; i < 4; i++) {
-        System.out.println("****");
-    }
-        
+    static int fibonacci(int n) {
+        if (n <= 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println("\n✨ Fibonacci Pattern:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+
+        System.out.println("\n\n🔥 Prime Numbers:");
+
+        for (int num = 2; num <= n; num++) {
+
+            boolean prime = true;
+
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+
+            if (prime) {
+                System.out.print(num + " ");
+            }
+        }
+
+        System.out.println("\n\n🎯 Star Pattern:");
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
 }
