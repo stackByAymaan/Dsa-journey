1b86895dccca0b1728b8828b8c735d0b8680deae
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-03 23:46:24 +0530

    Witcher

--- 04_Java Loops/Loops.java
+++ 04_Java Loops/Loops.java
@@ -2,11 +2,28 @@ public class Loops {
 
     public static void main(String[] args) {
 
-        System.out.println("             ______");
-        System.out.println("        ____/|_||_\\`.__");
-        System.out.println("       / _          _  \\");
-        System.out.println("      |_/ \\________/ \\__|");
-        System.out.println("        \\_/        \\_/");
-        System.out.println("          O        O");
+        System.out.println("             /\\");
+        System.out.println("            /  \\");
+        System.out.println("           / /\\ \\");
+        System.out.println("          / /  \\ \\");
+        System.out.println("         /_/____\\_\\");
+        System.out.println("            ||");
+        System.out.println("            ||");
+        System.out.println("        ____||____");
+        System.out.println("       /    ||    \\");
+        System.out.println("      /     ||     \\");
+        System.out.println("     /______||______\\");
+        System.out.println("        |   ||   |");
+        System.out.println("        |   ||   |");
+        System.out.println("        |___||___|");
+        System.out.println();
+        System.out.println("          THE WITCHER");
+        System.out.println("        _________");
+        System.out.println("       /         \\");
+        System.out.println("      /   WOLF    \\");
+        System.out.println("     <    HEAD     >");
+        System.out.println("      \\           /");
+        System.out.println("       \\_________/");
+
     }
 }
