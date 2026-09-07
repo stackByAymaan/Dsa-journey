8ee1caa97e48d3db4a7b41f799bdbd89b6a6e3d4
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-07 21:30:18 +0530

    Pattern Practice

--- 05_Pattern 1/pattern.java
+++ 05_Pattern 1/pattern.java
@@ -1,10 +1,59 @@
+// * Star Pattern
+
+// public class pattern {
+//     public static void main(String[] args) {
+//         for(int i = 1; i <= 5; i++) {
+//             for(int j =1; j <= i; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+//* Inverted Star Pattern
+
+// public class pattern {
+//     public static void main(String[] args) {
+//         for(int i = 1; i <= 5; i++) {
+//             for(int j =5; j >= i; j--) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+//* Half Pyramid Pattern
+
+// public class pattern {
+//     public static void main(String[] args) {
+//         for(int i=1; i<=4; i++) {
+//            for(int j = 1; j <= i; j++) {
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//            }
+//         }
+//     }
+
+
+
+//* Chracter Pattern 
+
 public class pattern {
     public static void main(String[] args) {
-        for(int i = 1; i <= 5; i++) {
-            for(int j =1; j <= i; j++) {
-                System.out.print("*");
+        char ch = 'A';
+        for(int i = 1; i <= 6; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(ch);
+                ch++;
             }
             System.out.println();
         }
     }
 }
+
+
