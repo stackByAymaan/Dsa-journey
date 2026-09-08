a6387618cc8d31a9f46431f67f13e146dc4b425f
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-09 01:31:37 +0530

    Functions demo

---
+++ 06_Functions and methods/main.java
@@ -0,0 +1,14 @@
+//^ Function/Methods
+
+public class Main {
+    public static void printHello() {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        return;
+    }
+    public static void main(String[] args) {
+        printHello();
+    }
+}
