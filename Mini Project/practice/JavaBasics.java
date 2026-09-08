112a690fba97b84aed19a90cad3efe2de584cecd
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-08 23:45:33 +0530

    error

--- Mini Project/practice/JavaBasics.java
+++ Mini Project/practice/JavaBasics.java
@@ -6,9 +6,7 @@ public class JavaBasics {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i = 1; i <= n; i++) {
-            if(i % 2 == 0) {
-                System.out.println(i);
+        
             }
         }
     }
