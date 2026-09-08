28d5539dcdd703cdeff74d6f37363f736eb6cb6b
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-09 01:00:25 +0530

    updated

--- Mini Project/practice/Main.java
+++ Mini Project/practice/Main.java
@@ -1,23 +1,14 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
-
-    for (int j = 1; j <= i; j++) {
-        System.out.print("*");
-    }
-
-    for (int j = 1; j <= 2 * (5 - i) - 1; j++) {
-        System.out.print(" ");
-    }
-
-    if (i < 5) {
-        for (int j = 1; j <= i; j++) {
-            System.out.print("*");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 0;
+        while (i <= n) {
+            System.out.print(i);
+            i++;
         }
     }
-
-    System.out.println();
 }
-        }
-    }
