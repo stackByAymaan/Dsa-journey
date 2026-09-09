f517114b30fb12112a818b9f3f2272af09c01db6
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-09 23:55:31 +0530

    Deleted pattern

--- 06_Functions and methods/main.java
+++ 06_Functions and methods/main.java
@@ -82,31 +82,19 @@
 //     }
 // }
 
+//? Binomial coefficient -> nCr = n! / (r! * (n-r)!) 
+// public class Main {
 
-public class Main {
-    public static void main(String[] args) {
+//     public static int factorial(int n) {
+//         int f = 1;
+//         for(int i = 1; i <= n; i++) {
+//             f = f * i;
+//         }
+//         return f;
+//     }
 
-        int n = 7;
-
-        for (int i = 1; i <= n; i++) {
-
-            for (int j = 1; j <= n; j++) {
-
-                if (i == j || i + j == n + 1) {
-                    System.out.print("* ");
-                } 
-                else if (i == 1 || i == n || j == 1 || j == n) {
-                    System.out.print("# ");
-                } 
-                else if ((i * j) % 3 == 0) {
-                    System.out.print("+ ");
-                } 
-                else {
-                    System.out.print(". ");
-                }
-            }
-
-            System.out.println();
-        }
-    }
-}
+//     public static void main(String[] args) {
+//        System.out.println(factorial(5));
+        
+//     }
+// }
