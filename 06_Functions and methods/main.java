251875ce8242c0256f68b5cd094fa7d3ed09bcd9
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-09 11:37:56 +0530

    Sum of 2 number

--- 06_Functions and methods/main.java
+++ 06_Functions and methods/main.java
@@ -1,14 +1,24 @@
 //^ Function/Methods
 
+import java.util.Scanner;
+
 public class Main {
-    public static void printHello() {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        return;
+    public static int calculateSum( int a, int b) { //parameter or formal parameter
+        int sum = a + b;    
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+       
+        return sum;
     }
+
     public static void main(String[] args) {
-        printHello();
+        System.out.print("Enter a:");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print("Enter b:");
+        int b = sc.nextInt();
+        int sum = calculateSum(a , b);  //argument or actual parameter
+
+
+        sc.close();
     }
 }
