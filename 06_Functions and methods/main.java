12b5c7765fa4bb46143ef908acf0bec5a313b750
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-13 20:31:02 +0530

    Java.main

--- 06_Functions and methods/main.java
+++ 06_Functions and methods/main.java
@@ -123,6 +123,29 @@
 
 
 //? Check if number is prime or not
+// public class Main {
+
+//     public static boolean isPrime(int n) {
+//     boolean isPrime = true;
+//     for(int i = 2; i<=n-1; i++) {
+//         if(n % i == 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//     return isPrime;
+//     }
+
+
+//     public static void main(String[] args) {
+//         System.out.println(isPrime(7));  
+//         System.out.println(isPrime(4));  
+//         System.out.println(isPrime(11));  
+//     }
+// }
+
+
+//* Optimized Prime number code 
 public class Main {
 
     public static boolean isPrime(int n) {
