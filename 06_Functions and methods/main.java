3950ad2efc2f3b6c140203842569dd55230bc33d
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-18 12:31:12 +0530

    Optimized Prime number code

--- 06_Functions and methods/main.java
+++ 06_Functions and methods/main.java
@@ -148,17 +148,21 @@
 //* Optimized Prime number code 
 public class Main {
 
-    public static boolean isPrime(int n) {
-    boolean isPrime = true;
-    for(int i = 2; i<=n-1; i++) {
-        if(n % i == 0) {
-            isPrime = false;
-            break;
-        }
-    }
-    return isPrime;
+public static boolean isPrime(int n) {
+
+    if(n == 2) {
+        return true;
     }
 
+    for(int i = 2; i <= Math.sqrt(n); i++) {
+
+        if(n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
     public static void main(String[] args) {
         System.out.println(isPrime(7));  
