6056b132f3674c2b3f8011b51ea505c55940ebc7
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-19 13:32:01 +0530

    Print all primes in a range

--- 06_Functions and methods/main.java
+++ 06_Functions and methods/main.java
@@ -146,27 +146,64 @@
 
 
 //* Optimized Prime number code 
+// public class Main {
+
+// public static boolean isPrime(int n) {
+
+//     if(n == 2) {
+//         return true;
+//     }
+
+//     for(int i = 2; i <= Math.sqrt(n); i++) {
+
+//         if(n % i == 0) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+//     public static void main(String[] args) {
+//         System.out.println(isPrime(7));  
+//         System.out.println(isPrime(4));  
+//         System.out.println(isPrime(11));  
+//     }
+// }
+
+
+//* Print all primes ina range
+
 public class Main {
+    public static boolean isPrime(int n) {
 
-public static boolean isPrime(int n) {
+        if(n == 2) {
+            return true;
+        }
 
-    if(n == 2) {
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+
+            if(n % i == 0) {
+                return false;
+            }
+        }
+
         return true;
     }
 
-    for(int i = 2; i <= Math.sqrt(n); i++) {
+    public static void primeInRange(int n) {
 
-        if(n % i == 0) {
-            return false;
+        for(int i = 2; i <= n; i++) {
+
+            if(isPrime(i)) {
+                System.out.print(i + " ");
+            }
         }
+        System.out.println();
     }
-
-    return true;
-}
-
     public static void main(String[] args) {
-        System.out.println(isPrime(7));  
-        System.out.println(isPrime(4));  
-        System.out.println(isPrime(11));  
+
+        System.out.println(isPrime(7));
+        primeInRange(30);
     }
 }
