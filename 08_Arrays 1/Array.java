9e785771d1b714aa9c87049549f61caf043d08d3
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-21 23:15:34 +0530

    Creating Array

--- 08_Arrays 1/Array.java
+++ 08_Arrays 1/Array.java
@@ -1,5 +1,15 @@
-public class Array {
-    public static void main(String[] args) {
-        
-    }
-}
+//^ Array 
+
+//* Creating Array
+// public class Array {
+//     public static void main(String[] args) {
+//         int marks[] = new int[100];
+
+//         int number[] = { 1, 2, 3};
+
+//         String fruits[] = {"Apple", "Orange"," Papaya"};
+//     }
+// }
+
+
+
