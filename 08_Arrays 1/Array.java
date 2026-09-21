bbca83ef74862d08d6a0596e048282242a73d4fd
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-21 23:16:49 +0530

    Performing Operations

--- 08_Arrays 1/Array.java
+++ 08_Arrays 1/Array.java
@@ -12,4 +12,25 @@
 // }
 
 
+//* Performing Operations
 
+import java.util.*;
+public class Array {
+    public static void main(String[] args) {
+
+        int marks[] = new int [5];
+
+        System.out.print("Enter your Marks in Physics chemistry & Bio respectively : ");
+        Scanner sc = new Scanner(System.in);
+
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println("phy : " + marks[0]);
+        System.out.println("chem : " + marks[1]);
+        System.out.println("Bio : " + marks[2]);
+
+        sc.close();
+    }
+}
