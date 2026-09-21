d3b6222966aa8a2d210213302516de4fae983593
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-21 23:50:55 +0530

    Passing Arrays as an argument

--- 08_Arrays 1/Array.java
+++ 08_Arrays 1/Array.java
@@ -14,30 +14,52 @@
 
 //* Performing Operations
 
+// import java.util.*;
+// public class Array {
+//     public static void main(String[] args) {
+
+//         int marks[] = new int [5];
+
+//         System.out.print("Enter your Marks in Physics chemistry & Bio respectively : ");
+//         Scanner sc = new Scanner(System.in);
+
+//         marks[0] = sc.nextInt();
+//         marks[1] = sc.nextInt();
+//         marks[2] = sc.nextInt();
+
+//         System.out.println("phy : " + marks[0]);
+//         System.out.println("chem : " + marks[1]);
+//         System.out.println("Bio : " + marks[2]);
+
+//         //Update
+//         // marks[2] = 100;
+//         marks[2] = marks[2] + 20;
+//         System.out.println("Updated marks in Bio : " + marks[2]);
+
+//         System.out.println("length of array = " + marks.length);
+
+//         sc.close();
+//     }
+// }
+
+//* Passing Arrays as an argument
+
 import java.util.*;
 public class Array {
-    public static void main(String[] args) {
+    public static void update (int marks[]) {
+        for(int i = 0; i <marks.length; i++) {
+            marks[i] = marks[i] + 1;
+        }
+    }
+    public static void main(String args[]) {
+        int marks[] = {97, 98, 99};
+        update(marks);
 
-        int marks[] = new int [5];
-
-        System.out.print("Enter your Marks in Physics chemistry & Bio respectively : ");
-        Scanner sc = new Scanner(System.in);
-
-        marks[0] = sc.nextInt();
-        marks[1] = sc.nextInt();
-        marks[2] = sc.nextInt();
-
-        System.out.println("phy : " + marks[0]);
-        System.out.println("chem : " + marks[1]);
-        System.out.println("Bio : " + marks[2]);
-
-        //Update
-        // marks[2] = 100;
-        marks[2] = marks[2] + 20;
-        System.out.println("Updated marks in Bio : " + marks[2]);
-
-        System.out.println("length of array = " + marks.length);
-
-        sc.close();
+        //print our marks
+        for(int i = 0; i<marks.length; i++) {
+            System.out.print(marks[i] + " ");
+        }
+        System.out.println();
     }
 }
+
