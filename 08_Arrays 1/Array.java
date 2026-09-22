e31bfa265c796ae80645065f3b95b824d96fcb79
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-22 10:50:25 +0530

    Largest/Smallest Number

--- 08_Arrays 1/Array.java
+++ 08_Arrays 1/Array.java
@@ -92,11 +92,31 @@
 // }
 
 
-//* Largest Number
+//* Largest/Smallest Number
 
-public class Array {
-     
-    public static void main(String args[]) {
-        
-    }
-}
+// public class Array {
+
+//     public static int getLargest(int numbers[]) {
+
+//         int largest = Integer.MIN_VALUE;   // -infinity
+//         int smallest = Integer.MAX_VALUE;   
+//         for(int i = 0; i < numbers.length; i++) {
+//             if(largest < numbers[i]) {
+//                 largest = numbers[i];
+//             }
+//             if(smallest > numbers[i]) {
+//                 smallest = numbers[i];
+//             }
+//         }
+//         System.out.println("smallest value : " + smallest);
+//         return largest;
+//     }
+
+//     public static void main(String args[]) {
+
+//         int number[] = {1, 2, 6, 3, 5};
+//         System.out.println("largest value : " + getLargest(number));
+//     }
+// }
+
+
