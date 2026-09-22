459bbcf68c5a852273765799be061c6ca57d42f7
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-22 23:32:19 +0530

    updated java

--- 08_Arrays 1/Array.java
+++ 08_Arrays 1/Array.java
@@ -159,27 +159,35 @@
 
 //* Reverse an Arrays
 
-public class Array {
-    public static void reverse(int numbers[]) {
-           int first = 0 , last = numbers.length - 1;
-           while(first < last) {
+// public class Array {
+//     public static void reverse(int numbers[]) {
+//            int first = 0 , last = numbers.length - 1;
+//            while(first < last) {
 
-            //swap
-            int temp = numbers[last];
-            numbers[last] = numbers[first];
-            numbers[first] = temp;
-            first++;
-            last--;
-           }
-    }
+//             //swap
+//             int temp = numbers[last];
+//             numbers[last] = numbers[first];
+//             numbers[first] = temp;
+//             first++;
+//             last--;
+//            }
+//     }
+//          public static void main(String[] args) {
+  
+//             int numbers[] = { 2, 4, 6, 8, 10 };
+//             reverse(numbers);
+
+//             for(int i = 0; i<numbers.length; i++) {
+//                 System.out.print(numbers[i] + " ");
+//             }
+//             System.out.println();
+//     }
+// }
+
+//* Pairs in an Array
+
+public class Array {
          public static void main(String[] args) {
   
-            int numbers[] = { 2, 4, 6, 8, 10 };
-            reverse(numbers);
-
-            for(int i = 0; i<numbers.length; i++) {
-                System.out.print(numbers[i] + " ");
-            }
-            System.out.println();
     }
 }
