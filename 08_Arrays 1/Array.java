789c471fe38bc27f3ed8e803cffc324f29cc985a
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-22 11:19:21 +0530

    Binary Search

--- 08_Arrays 1/Array.java
+++ 08_Arrays 1/Array.java
@@ -120,3 +120,37 @@
 // }
 
 
+//* Binary Search
+
+public class Array {
+    public static int binarySearch(int numbers[], int key) {
+
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            //comparison
+            if(numbers[mid] == key) {
+                return mid;
+            }
+
+            if(numbers[mid] < key) { //right
+                start = mid + 1;
+            } else {   //left
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+    public static void main(String[] args) {
+
+        int numbers[] = {2, 4, 6, 8, 10, 12, 14};
+        int key = 10;
+
+        int index = binarySearch(numbers, key);
+        System.out.println("key is at index : " + index);
+    }
+}
