c4effe74cc7346c310c1cbf05e7211bcd3d2ca90
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-26 00:41:42 +0530

    Pairs in an Array

--- 08_Arrays 1/Array.java
+++ 08_Arrays 1/Array.java
@@ -187,7 +187,18 @@
 //* Pairs in an Array
 
 public class Array {
-         public static void main(String[] args) {
-  
+    public static void printPairs(int number[]) {
+        for(int i= 0; i<number.length; i++) {
+            int curr = number[i];
+            for(int j = i+1; j<number.length; j++) {
+                System.out.print("(" + curr + " ," + number[i] + ")");
+            }
+            System.out.println();
+        }
+
+    }
+             public static void main(String[] args) {
+                int number[] = {2, 4, 6, 8, 10};
+                printPairs(number);
     }
 }
